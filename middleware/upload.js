72ae948d359a4bdf231fd96333bc6c59b4c64f0f
Stas Klymenko <klymenko.stas@gmail.com>
2025-09-08 12:01:42 +0300

    Update upload for Render

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,16 +1,24 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs"
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const dir = path.join(__dirname, "../tmp/uploads");
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const dir = path.join(__dirname, "../tmp/uploads");
 
-if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+// if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+const isRender = process.env.RENDER === "true";
+const baseTmpDir = isRender ? "/tmp" : path.join(process.cwd(), "tmp");
+const uploadDir = path.join(baseTmpDir, "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {cb(null, dir)},
+    destination: (req, file, cb) => {cb(null, uploadDir)},
     filename: (req, file, cb) => {cb(null, file.originalname)},
 })
 
